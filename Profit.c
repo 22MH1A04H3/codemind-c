97b163caf1e0d0859377b66c3ae0c543db618d1b
22MH1A04H3 <119719005+22MH1A04H3@users.noreply.github.com>
2022-12-05 18:09:12 +0530

    #include<stdio.h> int main() {     int sp,cp;     scanf("%d%d",&sp,&cp);     if(cp>sp)     {         printf("Profit");     }     else     {         printf("Loss");     }     return 0; }

---
+++ Profit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int sp,cp;
+    scanf("%d%d",&sp,&cp);
+    if(cp>sp)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+    return 0;
+}
